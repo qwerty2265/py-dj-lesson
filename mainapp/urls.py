375e20441090b8e27889a1cf35acbc940fb14392
Maks <maks_mun@bk.ru>
2024-06-06 00:19:09 +0500

    Add views and urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import index, products
+from .views import index, products, product, about, contacts
 
 urlpatterns = [
     path('', index),
     path('products/', products),
+    path('product/<int:product_id>/', product),
+    path('about/', about),
+    path('contacts/', contacts)
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -7,3 +7,12 @@ def index(request):
 
 def products(request):
     return render(request, 'products.html')
+
+def product(request, product_id):
+    return render(request, 'product.html', {'product_id': product_id})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
